c797f3f4ccd914f07ddaebdacf2fede4b806cf3c
dfinke <finked@hotmail.com>
2020-12-20 12:47:41 -0500

    Add unit tests

---
+++ __tests__/ghModule.tests.ps1
@@ -0,0 +1,29 @@
+Describe "Test gh psm1" {
+    BeforeAll {
+        # if (!(Get-Module -list Microsoft.PowerShell.Crescendo)) {
+        #     Install-Module -Name Microsoft.PowerShell.Crescendo -Force
+        # }
+        
+        $ConfigurationFile = "$PSScriptRoot\..\gh*.crescendo.json" 
+        $ConfigurationFile | Out-Host
+
+        $ModuleName = "Testdrive:\gh.psm1"
+        $ModuleName | Out-Host
+        
+        Export-CrescendoModule -ConfigurationFile $ConfigurationFile -ModuleName $ModuleName -Force
+        Import-Module $ModuleName -Force
+    }
+    Context "Test code generation worked" {
+        It "Tests all functions were created" {
+            $actual = Get-Command -Module gh | Sort-Object name
+            
+            $actual.Count | Should -Be 5
+            
+            $actual[0].name | Should -BeExactly 'Get-GHGist'
+            $actual[1].name | Should -BeExactly 'Get-GHIssue'
+            $actual[2].name | Should -BeExactly 'Get-GHPR'
+            $actual[3].name | Should -BeExactly 'Get-GHRelease'
+            $actual[4].name | Should -BeExactly 'Show-GHRepoOnWeb'
+        }
+    }
+}
